dea2a6e9c12638b640c29c82d0d557705aa64c85
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2024-10-30 10:57:56 +0300

    10 / 30 / 2024 -- Commit 1

---
+++ HousesOfTheFuture_Dapper_UI/ViewComponents/Layout/_NavbarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HousesOfTheFuture_Dapper_UI.ViewComponents.Layout
+{
+    public class _NavbarViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
