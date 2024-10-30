e3a9c5f33abbb8841e420dd5ab2be3ba0b0382a8
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2024-10-30 12:39:34 +0300

    10 / 30 / 2024 -- commit 2

---
+++ HousesOfTheFuture_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HousesOfTheFuture_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultHomePageProductList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
