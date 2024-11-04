8c5f1e19fa79bbb255812eb7d324a68f6bceb0cb
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2024-11-04 09:57:04 +0300

    11 / 4 / 2024 -- comiit 1

---
+++ HousesOfTheFuture_Dapper_Api/Repositories/BottomGridRepositories/BottomGridRepository.cs
@@ -0,0 +1,32 @@
+﻿using HousesOfTheFuture_Dapper_Api.Dtos.BottomGridDtos;
+
+namespace HousesOfTheFuture_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public class BottomGridRepository : IBottomGridRepository
+    {
+        public void CreateBottomGrid(CreateBottomGridDto createBottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultBottomGridDto>> GetAllBottomGridAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetBottomGridDto> GetBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HousesOfTheFuture_Dapper_Api/Repositories/BottomGridRepositories/IBottomGridRepository.cs
@@ -0,0 +1,14 @@
+﻿using HousesOfTheFuture_Dapper_Api.Dtos.BottomGridDtos;
+using HousesOfTheFuture_Dapper_Api.Dtos.ServiceDtos;
+
+namespace HousesOfTheFuture_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public interface IBottomGridRepository
+    {
+        Task<List<ResultBottomGridDto>> GetAllBottomGridAsync();
+        void CreateBottomGrid(CreateBottomGridDto createBottomGridDto);
+        void DeleteBottomGrid(int id);
+        void UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto);
+        Task<GetBottomGridDto> GetBottomGrid(int id);
+    }
+}
