48072fff535dbdb5a7010fde646951e90af158e0
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2024-11-06 09:54:13 +0300

    Kategoriler Admin Paneline Getirildi. -- commit 1

--- HousesOfTheFuture_Dapper_UI/Controllers/ProductController.cs
+++ HousesOfTheFuture_Dapper_UI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using HousesOfTheFuture_Dapper_UI.DTOS.ProductDtos;
+﻿using HousesOfTheFuture_Dapper_UI.DTOS.CategoryDtos;
+using HousesOfTheFuture_Dapper_UI.DTOS.ProductDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace HousesOfTheFuture_Dapper_UI.Controllers
@@ -30,6 +32,19 @@ namespace HousesOfTheFuture_Dapper_UI.Controllers
         [HttpGet]
         public async Task<IActionResult> CreateProduct()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44314/api/Categories");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+
+            List<SelectListItem> categoryValues = (from x in values.ToList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryID.ToString()
+                                               }).ToList();
+            ViewBag.v = categoryValues;
             return View();
         }
     }
