2ef362616b351197ef11de11e26d7c38781c24b7
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2024-11-10 19:04:33 +0300

    Popular Location ların Views Ları Eklendi -- commit 2

---
+++ HousesOfTheFuture_Dapper_UI/DTOS/PopularLocationDtos/UpdatePopularLocationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HousesOfTheFuture_Dapper_UI.DTOS.PopularLocationDtos
+{
+    public class UpdatePopularLocationDto
+    {
+        public int LocationID { get; set; }
+        public string CityName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
