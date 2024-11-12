126b6df3c9485c7a8055255dd186d93b7a652942
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2024-11-12 16:46:43 +0300

    DashboardC ComponentPartial View Kısmı Eklendi -- commit  6

---
+++ HousesOfTheFuture_Dapper_UI/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HousesOfTheFuture_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardLast5ProductComponentPartial:ViewComponent
+    {
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
