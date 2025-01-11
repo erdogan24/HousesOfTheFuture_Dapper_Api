d21d4ca07de7edca33634719943e2781754db987
Erdoğan Akpolat <erdoganakpolat98@gmail.com>
2025-01-11 20:03:30 +0300

    Property Controller Added

---
+++ HousesOfTheFuture_Dapper_UI/Controllers/PropertyController.cs
@@ -0,0 +1,35 @@
+﻿using HousesOfTheFuture_Dapper_UI.DTOS.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HousesOfTheFuture_Dapper_UI.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PropertyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44314/api/Products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> PropertySingle(int id)
+        {
+            return View();
+        }
+    }
+}
